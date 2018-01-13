grimreaper/freebsd-git-svn

commit 4c4c8536e53dcb9107022c40f98c9a00d5570f7e

- There is no need to keep the tuning error and re-tuning interrupts   enabled (though, no interrupt generation enabled for them) all the   time as soon as (re-)tuning is supported; only enable them and let   them generate interrupts when actually using (re-)tuning. - Also disable all interrupts except SDHCI_INT_DATA_AVAIL ones while   executing tuning and not just their signaling.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@327339 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/sdhci/sdhci.c
@@ -301,7 +301,7 @@ sdhci_init(struct sdhci_slot *slot)
 		slot->intmask |= SDHCI_INT_CARD_REMOVE | SDHCI_INT_CARD_INSERT;
 	}
 
-	WR4(slot, SDHCI_INT_ENABLE, slot->intmask | sdhci_tuning_intmask(slot));
+	WR4(slot, SDHCI_INT_ENABLE, slot->intmask);
 	WR4(slot, SDHCI_SIGNAL_ENABLE, slot->intmask);
 }
 
@@ -654,6 +654,7 @@ sdhci_card_task(void *arg, int pending __unused)
 			xpt_rescan(ccb);
 #else
 			slot->intmask &= ~sdhci_tuning_intmask(slot);
+			WR4(slot, SDHCI_INT_ENABLE, slot->intmask);
 			WR4(slot, SDHCI_SIGNAL_ENABLE, slot->intmask);
 			slot->opt &= ~SDHCI_TUNING_ENABLED;
 			SDHCI_UNLOCK(slot);
@@ -1338,6 +1339,7 @@ sdhci_generic_tune(device_t brdev __unused, device_t reqdev, bool hs400)
 	if (err == 0) {
 		slot->opt |= SDHCI_TUNING_ENABLED;
 		slot->intmask |= sdhci_tuning_intmask(slot);
+		WR4(slot, SDHCI_INT_ENABLE, slot->intmask);
 		WR4(slot, SDHCI_SIGNAL_ENABLE, slot->intmask);
 		if (slot->retune_ticks) {
 			callout_reset(&slot->retune_callout, slot->retune_ticks,
@@ -1406,6 +1408,7 @@ sdhci_exec_tuning(struct sdhci_slot *slot, bool reset)
 	 */
 	intmask = slot->intmask;
 	slot->intmask = SDHCI_INT_DATA_AVAIL;
+	WR4(slot, SDHCI_INT_ENABLE, SDHCI_INT_DATA_AVAIL);
 	WR4(slot, SDHCI_SIGNAL_ENABLE, SDHCI_INT_DATA_AVAIL);
 
 	hostctrl2 = RD2(slot, SDHCI_HOST_CONTROL2);
@@ -1438,6 +1441,7 @@ sdhci_exec_tuning(struct sdhci_slot *slot, bool reset)
 
 	slot->opt = opt;
 	slot->intmask = intmask;
+	WR4(slot, SDHCI_INT_ENABLE, intmask);
 	WR4(slot, SDHCI_SIGNAL_ENABLE, intmask);
 
 	if ((hostctrl2 & (SDHCI_CTRL2_EXEC_TUNING |

commit 2aa180efd40fc9c492f91b52052bb9dba95c830d

Fix a bug introduced in r327339; at the point in time re-tuning is executed, the interrupt aggregation code might have disabled the SDHCI_INT_DMA_END and/or SDHCI_INT_RESPONSE bits in slot->intmask and the SDHCI_SIGNAL_ENABLE register respectively. So when restoring the interrupt masks based on the previous contents of slot->intmask in sdhci_exec_tuning(), ensure that the SDHCI_INT_ENABLE register doesn't lose these two bits. While at it and in the spirit of r327339, let sdhci_tuning_intmask() set the tuning error and re-tuning interrupt bits based on the SDHCI_TUNING_ENABLED rather than the SDHCI_TUNING_SUPPORTED flag being set, i. e. only when (re-)tuning is actually used. Currently, this changes makes no net difference, though.
git-svn-id: svn+ssh://svn.freebsd.org/base/head@327924 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/sdhci/sdhci.c
@@ -273,7 +273,7 @@ sdhci_tuning_intmask(struct sdhci_slot *slot)
 	uint32_t intmask;
 
 	intmask = 0;
-	if (slot->opt & SDHCI_TUNING_SUPPORTED) {
+	if (slot->opt & SDHCI_TUNING_ENABLED) {
 		intmask |= SDHCI_INT_TUNEERR;
 		if (slot->retune_mode == SDHCI_RETUNE_MODE_2 ||
 		    slot->retune_mode == SDHCI_RETUNE_MODE_3)
@@ -1439,9 +1439,17 @@ sdhci_exec_tuning(struct sdhci_slot *slot, bool reset)
 			DELAY(1000);
 	}
 
+	/*
+	 * Restore DMA usage and interrupts.
+	 * Note that the interrupt aggregation code might have cleared
+	 * SDHCI_INT_DMA_END and/or SDHCI_INT_RESPONSE in slot->intmask
+	 * and SDHCI_SIGNAL_ENABLE respectively so ensure SDHCI_INT_ENABLE
+	 * doesn't lose these.
+	 */
 	slot->opt = opt;
 	slot->intmask = intmask;
-	WR4(slot, SDHCI_INT_ENABLE, intmask);
+	WR4(slot, SDHCI_INT_ENABLE, intmask | SDHCI_INT_DMA_END |
+	    SDHCI_INT_RESPONSE);
 	WR4(slot, SDHCI_SIGNAL_ENABLE, intmask);
 
 	if ((hostctrl2 & (SDHCI_CTRL2_EXEC_TUNING |